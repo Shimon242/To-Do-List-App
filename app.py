a9543b56116f41616f65d5bf9828a017d2b367a5
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2025-08-23 00:30:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
